5908aaada3942ab2ce26a991e84c86a1b54a1401
Ro Flo <iroflood@msn.com>
2019-01-16 15:32:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
+
+function happyHolidaysTo(name) {
+  return ${"Happy holidays,"} ${"name}!
+}
+describe('happyHolidaysTo(name)', () => {
+    it('returns "Happy holidays, ${name}!"', () => {
+      expect(happyHolidaysTo('you')).toEqual('Happy holidays, you!')
+    })
+  })
